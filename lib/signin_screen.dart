3bb658f81e5c6fa650a5ef1f1d50bbd14d8f5030
clebemachado <clebmachado.em@gmail.com>
2022-01-12 10:33:35 -0300

    Correção do Erro Botton Overflow

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:login_ui/constants.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -7,118 +8,124 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/person.jpg"),
-                    fit: BoxFit.cover,
-                    alignment: Alignment.bottomCenter),
+      body: SingleChildScrollView(
+        reverse:true,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/person.jpg"),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.bottomCenter),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
                     children: [
-                      Text(
-                        "SIGN IN",
-                        style: Theme.of(context).textTheme.headline4,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "SIGN IN",
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                          Text(
+                            "SIGN UP",
+                            style: Theme.of(context).textTheme.button,
+                          )
+                        ],
                       ),
-                      Text(
-                        "SIGN UP",
-                        style: Theme.of(context).textTheme.button,
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 30),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.only(right: 8.0),
-                          child: Icon(
-                            Icons.alternate_email,
-                            color: kPrimaryColor,
-                          ),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            decoration:
-                                InputDecoration(hintText: "Email Address"),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
+                      const Spacer(),
                       Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.lock,
-                          color: kPrimaryColor,
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: Icon(
+                                Icons.alternate_email,
+                                color: kPrimaryColor,
+                              ),
+                            ),
+                            Expanded(
+                              child: TextField(
+                                decoration:
+                                    InputDecoration(hintText: "Email Address"),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(hintText: "Password"),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.only(right: 8.0),
+                            child: Icon(
+                              Icons.lock,
+                              color: kPrimaryColor,
+                            ),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(hintText: "Password"),
+                            ),
+                          )
+                        ],
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(color: Colors.white.withOpacity(0.5)),
+                              ),
+                              child: Icon(Icons.android,
+                                  color: Colors.white.withOpacity(0.5)),
+                            ),
+                            const SizedBox(width: 16),
+                            Container(
+                              padding: const EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(color: Colors.white.withOpacity(0.5)),
+                              ),
+                              child: Icon(Icons.chat,
+                                  color: Colors.white.withOpacity(0.5)),
+                            ),
+                            const Spacer(),
+                            Container(
+                              padding: const EdgeInsets.all(16),
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: kPrimaryColor),
+                              child: const Icon(Icons.arrow_forward, color: Colors.black,),
+                            )
+                          ],
                         ),
                       )
                     ],
                   ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 30),
-                    child: Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border:
-                                Border.all(color: Colors.white.withOpacity(0.5)),
-                          ),
-                          child: Icon(Icons.android,
-                              color: Colors.white.withOpacity(0.5)),
-                        ),
-                        const SizedBox(width: 16),
-                        Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border:
-                                Border.all(color: Colors.white.withOpacity(0.5)),
-                          ),
-                          child: Icon(Icons.chat,
-                              color: Colors.white.withOpacity(0.5)),
-                        ),
-                        const Spacer(),
-                        Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: const BoxDecoration(
-                              shape: BoxShape.circle, color: kPrimaryColor),
-                          child: const Icon(Icons.arrow_forward, color: Colors.black,),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
